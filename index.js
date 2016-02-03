0c6f30466db26d3b06587f916d8bb600c78c0ae5
John Xiao <xiao-jianfeng@qq.com>
2016-02-03 14:41:08 +0800

    Clean up file

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var PLUGIN_NAME = 'gulp-watch-less2';
-
 var path = require('path');
 var gutil = require('gulp-util');
 var Duplex = require('readable-stream').Duplex;
@@ -13,48 +11,49 @@ var chokidar = require('chokidar');
 var Glob = require('glob').Glob;
 var less = require('less');
 
-var _importIndex = Object.create(null);
-var _lessIndex = Object.create(null);
-
+var PLUGIN_NAME = 'gulp-watch-less2';
 // Name of the event fired when @imports cause file to change
-// (Overwrites the current file.event set by gulp-watch/gaze)
 var changedByImptEvent = 'changed:by:import';
 
-module.exports = function (glob, options, callback) {
+
+module.exports = function (glob, opts, callback) {
   // No-op callback if not given
-  if (!options) {
-    options = {};
+  if (!opts) {
+    opts = {};
   }
   if (!callback) {
     callback = function () {};
   }
 
   // Merge defaults
-  options = mergeDefaults(options, {
+  opts = mergeDefaults(opts, {
     // ignoreInitial: false,
     name: 'watchLess',
     less: {},
     ignoreInitial: true
   });
-  options.events = ['add', 'change', 'unlink'];
+  opts.events = ['add', 'change', 'unlink'];
 
   // used for push file base
-  if (!options.base) {
-    options.base = glob2base(new Glob(glob));
+  if (!opts.base) {
+    opts.base = glob2base(new Glob(glob));
   }
+
+  // Index each imports's parents, used to trigger changed:by:import event
+  var _triggerIndex = Object.create(null);
+  // Index imports by less files
+  var _compileIndex = Object.create(null);
 
   // Generate a basic `gulp-watch` stream
   // Listen to `add, change, unlink` events
-  // var watchStream = watch(glob, options, callback)
+  // var watchStream = watch(glob, opts, callback)
   var watchStream = new Duplex({objectMode: true, allowHalfOpen: true});
 
   watchStream._write = function _write(file, enc, done) {
     callback(file);
-    // watchLessImports(file, options, importCallback);
-    // console.log('_write', file.event)
-    // event is undefined for ignored initial
+    // event is undefined when ignored initial
     if (!file.event) {
-      initWatchImpt(file);
+      parseImports(file);
     }
 
     watchStream.push(file);
@@ -65,16 +64,15 @@ module.exports = function (glob, options, callback) {
     gutil.log(gutil.colors.red(PLUGIN_NAME + ' Error') , msg)
   })
 
-  var watcher = chokidar.watch(glob, options)
+  var watcher = chokidar.watch(glob, opts)
                         .on('all', watchHandler)
-                        // Note: ready is no use at all cause watch.add imports
+                        // Note: ready is no use at all cause watch will add imports when stream emit ready
                         // .on('ready', function () {
                         //   var count = 0;
                         //   var watchedPaths = watcher.getWatched();
                         //   Object.keys(watchedPaths).forEach(function (k) {
                         //     count += watchedPaths[k].length
                         //   })
-                        //   gutil.log(PLUGIN_NAME, 'is watching ', gutil.colors.magenta(count), 'less files')
                         // })
                         ;
 
@@ -83,47 +81,101 @@ module.exports = function (glob, options, callback) {
       watcher.on(ev, watchStream.emit.bind(watchStream, ev));
     });
   watchStream.add = function add(newGlobs) {
-    watcher.add(newGlobs, options);
+    watcher.add(newGlobs, opts);
+    // TODO: parseImports for newGlobs
   };
-	watchStream.close = function () {
+	watchStream.close = function (cb) {
 		watcher.close();
 		this.emit('end');
-	};
+    cb && cb();
+	}
 
-  // Generates list of @import paths for a given Vinyl file
-  function getLessFileImports(file, options, cb) {
-    // Support (file, cb) signature
-    if (typeof options === 'function') {
-      cb = options; options = null;
+  function parseImports(file) {
+    var filePath = file.path;
+    function handler(imports) {
+      // not collect those who have no imports
+      if (imports && imports.length) {
+        imports.forEach(function (impt) {
+          addImptIndexes(impt, filePath);
+        });
+
+        _compileIndex[filePath] = imports;
+        watcher.add(imports, opts);
+        watchStream.emit('importsReady', filePath)
+        _log('watching', filePath + ' imports');
+      }
+    }
+    getLessFileImports(file, handler);
+  }
+
+  function _streamPush(event, f) {
+    // event limit to ['add', 'change', 'changed:by:import']
+    f.event = event;
+    watchStream.emit(event);
+    watchStream.push(f);
+    callback(f);
+  }
+
+  function watchHandler(event, filePath) {
+    _log(event, filePath);
+    if (event == 'add') {
+      vinyl.read(filePath, opts).then(function (f) {
+        parseImports(f);
+        _streamPush(event, f);
+      });
+    }
+    else if(event == 'change') {
+      changeHandler(event, filePath);
+    }
+    else if(event == 'unlink') {
+        filePath = pathIsAbsolute(filePath) ? filePath : path.join(opts.cwd || process.cwd(), filePath);
+        removeTriggerIndexes(filePath);
+        watchStream.emit('unlink');
+    }
+  }
+
+  function changeHandler(event, filePath) {
+    if (event === 'change' && typeof _triggerIndex[filePath] !== 'undefined') {
+      var _thisTriggerList = _triggerIndex[filePath];
+      // recompile associated less
+      if (_thisTriggerList.length) {
+        Object.keys(_thisTriggerList).forEach(function (k) {
+          var lessFile = _thisTriggerList[k];
+          changeHandler(changedByImptEvent, lessFile);
+          _log(changedByImptEvent, lessFile);
+        });
+      }
+      return;
     }
 
-    // Parse the filepath, using file path as `filename` option
-    less.parse(file.contents.toString('utf8'), mergeDefaults({
-      filename: file.path
-    }, options || {}), function (err, root, imports) {
-      // Add a better error message / properties
-      if (err) {
-        err.lineNumber = err.line;
-        err.fileName = err.filename;
-        err.message = err.message + ' in file ' + err.fileName + ' line no. ' + err.lineNumber;
-
-        // Emit the error if one was returned
-        watchStream.emit('error', new gutil.PluginError(PLUGIN_NAME, err.message));
+    vinyl.read(filePath, opts).then(function (f) {
+      if (event == changedByImptEvent) {
+        event = 'change'
       }
       else {
-	      // Generate imports list from the files hash (sorted)
-	      var length = Object.keys(imports.files).length;
-	      imports = Object.keys(imports.files).sort();
-	      imports.length = length;
-
-	      cb(imports);
+        detectImptChange(f);
       }
-
+      _streamPush(event, f);
     });
   }
 
+  function addImptIndexes(impt, filePath, watch) {
+    if (typeof _triggerIndex[impt] === 'undefined') {
+      _triggerIndex[impt] = [];
+    }
+
+    var lessList = _triggerIndex[impt];
+    // no watched yet
+    if (typeof lessList[filePath] === 'undefined') {
+      lessList.push(filePath);
+      if (watch)
+        watcher.add(imptPath, opts);
+    }
+  }
+
+
   function removeImptIndexes(imptPath, filePath) {
-    var lessList = _importIndex[imptPath];
+    var lessList = _triggerIndex[imptPath];
     var pos = lessList.indexOf(filePath);
     // console.log(imptPath, filePath, pos)
     if (pos > -1) {
@@ -136,23 +188,64 @@ module.exports = function (glob, options, callback) {
     }
   }
 
-  function removeIndexes(filePath) {
-    if (typeof _lessIndex[filePath] !== 'undefined') {
-      delete _lessIndex[filePath];
-
-      // Delete less index from imports list
-      Object.keys(_importIndex).forEach(function (imptPath) {
-        removeImptIndexes(imptPath, filePath);
-      });
-      // console.log(_lessIndex)
-      // console.log(_importIndex)
-    }
+  function _colorMagenta(text) {
+    return gutil.colors.magenta(text);
   }
+
+  function _log(event, filePath) {
+    var msg;
+
+    if (!opts.verbose)
+      return;
+
+    filePath = filePath.replace(opts.cwd || process.cwd() + '/', '');
+    if(event == 'watching')
+      msg = ['is', event, _colorMagenta(filePath)];
+    else if (event == 'unwatched') {
+      msg = [event, _colorMagenta(filePath)];
+    }
+    else {
+      event = event[event.length - 1] === 'e' ? event + 'd' : event + 'ed';
+      msg = ['saw', _colorMagenta(filePath), event];
+    }
+
+    if (opts.name) {
+      msg.unshift(gutil.colors.cyan(opts.name));
+    }
+
+    gutil.log.apply(gutil, msg);
+  }
+
+  // Generates list of @import paths for a given Vinyl file
+  function getLessFileImports(file, cb) {
+    // Parse the filepath, using file path as `filename` option
+    less.parse(file.contents.toString('utf8'), mergeDefaults({
+      filename: file.path
+    }, opts || {}), function (err, root, imports) {
+      // Add a better error message / properties
+      if (err) {
+        err.lineNumber = err.line;
+        err.fileName = err.filename;
+        err.message = err.message + ' in file ' + err.fileName + ' line no. ' + err.lineNumber;
+
+        // Emit the error if one was returned
+        watchStream.emit('error', new gutil.PluginError(PLUGIN_NAME, err.message));
+      }
+      else {
+        // Generate imports list from the files hash (sorted)
+        var length = Object.keys(imports.files).length;
+        imports = Object.keys(imports.files).sort();
+        imports.length = length;
+        cb(imports);
+      }
+    });
+  }
+
 
   function detectImptChange(file) {
     var filePath = file.path;
     getLessFileImports(file, function (thisImports) {
-      var previousImpts = _lessIndex[filePath] || [];
+      var previousImpts = _compileIndex[filePath] || [];
       if (!thisImports) {
         thisImports = [];
       }
@@ -166,139 +259,30 @@ module.exports = function (glob, options, callback) {
             removeImptIndexes(imptPath, filePath);
           }
         });
+        // watch new imports
         thisImports.forEach(function (imptPath) {
           if (previousImpts.indexOf(imptPath) === -1) {
             // console.log('add new', imptPath)
             addImptIndexes(imptPath, filePath);
-            watcher.add(imptPath, options);
           }
         });
       }
 
-      _lessIndex[filePath] = thisImports;
+      _compileIndex[filePath] = thisImports;
     });
   }
 
-  function addImptIndexes(impt, filePath) {
-    if (typeof _importIndex[impt] === 'undefined') {
-      _importIndex[impt] = [];
+  function removeTriggerIndexes(filePath) {
+    if (typeof _compileIndex[filePath] !== 'undefined') {
+      delete _compileIndex[filePath];
+
+      // Delete less index from imports list
+      Object.keys(_triggerIndex).forEach(function (imptPath) {
+        removeImptIndexes(imptPath, filePath);
+      });
+      // console.log(_compileIndex)
+      // console.log(_triggerIndex)
     }
-
-    var lessList = _importIndex[impt];
-    // no watched yet
-    if (typeof lessList[filePath] === 'undefined') {
-      lessList.push(filePath);
-    }
-  }
-
-  function initWatchImpt(file) {
-    var filePath = file.path;
-    function handler(imports) {
-      if (imports && imports.length) {
-        // console.log(Object.keys(imports), filePath)
-        imports.forEach(function (impt) {
-          addImptIndexes(impt, filePath);
-        });
-
-        // not collect those who have no imports
-        _lessIndex[filePath] = imports;
-        // console.log('watching ', imports)
-        watcher.add(imports, options);
-        _log('watching', filePath + ' imports');
-        // console.log(_lessIndex)
-        // console.log(_importIndex)
-      }
-    }
-    getLessFileImports(file, handler);
-  }
-
-  function pushFile(event, filePath) {
-    // _log(event, filePath);
-
-    if (event === 'change' && typeof _importIndex[filePath] !== 'undefined') {
-      var relativeLess = _importIndex[filePath];
-      // recompile associated less
-      if (relativeLess.length) {
-        Object.keys(relativeLess).forEach(function (k) {
-          var lessFile = relativeLess[k];
-          pushFile(changedByImptEvent, lessFile);
-          _log(changedByImptEvent, lessFile);
-        });
-      }
-      return;
-    }
-
-    function _push(event, f) {
-      // event limit to ['add', 'change', 'changed:by:import']
-      f.event = event;
-      watchStream.emit(event);
-      watchStream.push(f);
-      callback(f);
-    }
-
-    vinyl.read(filePath, options).then(function (f) {
-      switch (event) {
-        case 'add':
-          initWatchImpt(f);
-          break;
-        case 'change':
-          detectImptChange(f);
-          break;
-        case changedByImptEvent:
-          event = 'change';
-          break;
-        default:
-          break;
-      }
-
-      _push(event, f);
-    });
-  }
-
-  function watchHandler(event, filePath) {
-    switch (event) {
-      case 'add':
-      case 'change':
-        _log(event, filePath);
-        pushFile(event, filePath);
-        break;
-      case 'unlink':
-        filePath = pathIsAbsolute(filePath) ? filePath : path.join(options.cwd || process.cwd(), filePath);
-        removeIndexes(filePath);
-        _log(event, filePath);
-        watchStream.emit('unlink');
-        break;
-      default:
-        break;
-    }
-  }
-
-  function _colorMagenta(text) {
-    return gutil.colors.magenta(text);
-  }
-
-  function _log(event, filePath) {
-    var msg;
-
-    if (!options.verbose)
-      return;
-
-    filePath = filePath.replace(options.cwd || process.cwd() + '/', '');
-    if(event == 'watching')
-      msg = ['is', event, _colorMagenta(filePath)];
-    else if (event == 'unwatched') {
-      msg = [event, _colorMagenta(filePath)];
-    }
-    else {
-      event = event[event.length - 1] === 'e' ? event + 'd' : event + 'ed';
-      msg = ['saw', _colorMagenta(filePath), event];
-    }
-
-    if (options.name) {
-      msg.unshift(gutil.colors.cyan(options.name));
-    }
-
-    gutil.log.apply(gutil, msg);
   }
 
   return watchStream;
