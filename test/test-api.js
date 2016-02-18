490fbaf8a4fbf08b3ab2a2032d899e7db24b5108
John Xiao <xiao-jianfeng@qq.com>
2016-02-18 19:11:56 +0800

    Fix test on node 4.2.4

--- test/test-api.js
+++ test/test-api.js
@@ -35,21 +35,19 @@ describe('api', function () {
   })
 
   it('should emit added file', function (done) {
-    watchStream = watchLess(fixtures('*/*.less'), {verbose: false});
-    watchStream.add(fixtures('*.less'));
-    watchStream.once('data', function (file) {
-      file.relative.should.eql('new.less');
-      file.event.should.eql('add');
-      watchStream.close(done);
-    })
-    .once('ready', touch(fixtures('new.less')));
+    watchStream = watchLess(fixtures('*.less'), {verbose: false});
+    watchStream
+      .once('data', function (file) {
+        file.relative.should.eql('new.less');
+        file.event.should.eql('add');
+        watchStream.close(done);
+      })
+      .once('ready', touch(fixtures('new.less')));
   });
 
   it('should emit change event on file change', function (done) {
-    fs.mkdirSync(fixtures('imports'));
-    fs.writeFileSync(fixtures('imports/1.less'), '.import {color: #fff;}');
     watchStream = watchLess(fixtures('*.less'), {verbose: false});
-    watchStream.once('ready', touch(fixtures('new.less'), '@import "imports/1";.change{display:none;}'));
+    watchStream.once('ready', touch(fixtures('new.less'), '.change{display:none;}'));
     watchStream.once('data', function (file) {
       file.relative.should.eql('new.less');
       file.event.should.eql('change');
@@ -57,26 +55,33 @@ describe('api', function () {
     });
   });
 
-  it('should emit change event on import change', function (done) {
-    watchStream = watchLess(fixtures('new.less'), {verbose: false});
-    var file = createVinyl('new.less');
-    watchStream.write(file);
+  it.only('should emit change event on import change', function (done) {
+    fs.mkdirSync(fixtures('imports'));
+    fs.writeFileSync(fixtures('imports/1.less'), '.import {color: #fff;}');
+    fs.writeFileSync(fixtures('new2.less'), '@import "imports/1";');
+
+    watchStream = watchLess(fixtures('new2.less'), {verbose: false, ignoreInitial: true});
+    watchStream
+      .write(createVinyl('new2.less'));
+
     watchStream
       .once('data', function (file) {
-        file.relative.should.eql('new.less');
+        file.relative.should.eql('new2.less');
         should.not.exist(file.event);
       })
-      .once('importsReady', function (filePath) {
-        // on new.less's imports are watched
-        if(filePath == fixtures('new.less')) {
-          this.once('data', function (file) {
-            file.relative.should.eql('new.less');
+      .once('importsReady', function (f) {
+        if(f == fixtures('new2.less')) {
+          watchStream.once('data', function (file) {
+            file.relative.should.eql('new2.less');
             file.event.should.eql('change');
             done();
           })
-          fs.writeFileSync(fixtures('imports/1.less'), '.import {color: #000;}');
+          // node 4.2.4 watcher.add is async opration, so the importsReady event is not reliable
+          setTimeout(function () {
+            fs.writeFileSync(fixtures('imports/1.less'), '.import {color: #aaa;}');
+          }, 1000);
         }
-      })
+      });
   });
 
 
